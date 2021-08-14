011ab5c5da7ac7a17b6cc7b1218283734d2a9e20
Rudie G <rauoldm@gmail.com>
2021-08-14 10:55:16 -0500

    wrote service-worker.js file and relevant logic in index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register("/service-worker.js")
+    .then((reg => {
+      console.log("we have registered our service worker", reg)
+    }))
+  })
+}
+
 let transactions = [];
 let myChart;
 
